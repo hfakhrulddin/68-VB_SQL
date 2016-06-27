2227abb1754af0ad01a994f2f937bdfaa2af449a
Hussen Fakhrulddin <hf556482@gmail.com>
2016-06-26 23:00:40 -0700

    inti

---
+++ WindowsApplication1/CURD_SQL.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Data.Sql
+Imports System.Data.SqlClient
+
+
+
+
+Public Class CURD_SQL
+    'Connection amd Command
+    Private DBCon As New SqlConnection("Server=MINECRAFT\SQLEXPRESS;Database=SQLApps;User=sa;Pwd=Password1;")
+    Private DBCmd As SqlCommand
+
+    ' DB DATA
+    Public DBDA As SqlDataAdapter
+    Public DBDT As DataTable
+
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+    Dim Sql As New CURD_SQL
+
+    Public Sub CreateUser()
+        ' VALIDATE REQUIRED FIELDS
+        If Len(txtUser.Text) < 6 Then MsgBox("Username too short.") : Exit Sub
+        If Len(txtPass.Text) < 8 Then MsgBox("Password too short.") : Exit Sub
+
+        ' SET INSERT PARAMS
+        Sql.AddParam("@user", txtUser.Text)
+        Sql.AddParam("@pass", txtPass.Text)
+        Sql.AddParam("@email", txtEmail.Text)
+        Sql.AddParam("@website", txtWebsite.Text)
+        Sql.AddParam("@active", CInt(cbActive.Checked))
+        Sql.AddParam("@admin", CInt(cbAdmin.Checked))
+
+        ' EXECUTE INSERT COMMAND
+        Sql.ExecQuery("INSERT INTO members (username,password,email,website,active,admin) " &
+                      "VALUES(@user,@pass,@email,@website,@active,@admin)")
+
+
+        ' REPORT ERRORS
+        If NotEmpty(Sql.Exception) Then MsgBox(Sql.Exception)
+    End Sub
+
+    Public Sub ReadUsers()
+        Sql.ExecQuery("SELECT username FROM members")
+
+        ' REPORT & ABORT ON ERRORS
+        If NotEmpty(Sql.Exception) Then MsgBox(Sql.Exception) : Exit Sub
+
+        ' POPULATE COMBOBOX WITH USERNAMES
+        cbxUsers.Items.Clear()
+        For Each r As DataRow In Sql.DBDT.Rows
+            cbxUsers.Items.Add(r("username"))
+        Next
+
+        ' SELECT FIRST USER IF USERS ARE FOUND
+        If cbxUsers.Items.Count > 0 Then cbxUsers.SelectedIndex = 0
+    End Sub
+
+
+
+
+
+
+
+End Class
